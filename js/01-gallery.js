affe6b900b90a0884f0e099458c3beff3e29a30a
MagdalenaBernat <magdalena.bernat@onet.pl>
2022-10-29 21:48:26 +0200

    changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('div.gallery'); 
+
+const createGallery = galleryItems
+  .map(
+    (item) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}" 
+            data-source="${item.original}"
+            alt="${item.description}"
+          />
+        </a>
+      </div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", createGallery);
+
+gallery.addEventListener("click", selectImage);
+
+function selectImage(event) {
+  if (event.target.nodeName !== "DIV") {
+    return;
+  }
+  const selectedImage = event.target.dataset.original;
+  console.log(selectedImage);
+};
+
+document.querySelector('div.gallery').onclick = () => {
+
+  basicLightbox.create(`
+		<img width="1400" height="900" src="https://placehold.it/1400x900">
+	`).show()
+};
+
+
+
+//const parent = document.querySelector("#parent");  // ?? nie wiem co chciałam xD
+
+
+
+console.log(gallery);
 console.log(galleryItems);
